2f24a877c74f5d8b7b3df9a28cddee0ce3d9cabe
KenTu <the.king.of.sky@gmail.com>
2026-06-04 11:36:53 +0800

    Force clasp push in helper script

--- push-to-google.cmd
+++ push-to-google.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-"%USERPROFILE%\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" "%APPDATA%\npm\node_modules\@google\clasp\build\src\index.js" push
+"%USERPROFILE%\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" "%APPDATA%\npm\node_modules\@google\clasp\build\src\index.js" push -f
 endlocal
